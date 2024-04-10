983d8fd1c7695003bcad7d150bddab91da7d1813
Dawit Mellese <dawitm777@gmail.com>
2024-04-10 14:41:21 +0300

    database connection

--- server/index.js
+++ server/index.js
@@ -1,8 +1,19 @@
-import express from 'express'
+import express from "express";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
 
+dotenv.config();
 const app = express();
 
+mongoose
+  .connect(process.env.MONGODB_URI)
+  .then(() => {
+    console.log("Database is connected..");
+  })
+  .catch((err) => {
+    console.log(err);
+  });
 
 app.listen(3000, () => {
-    console.log("Server is running in 3000");
-})
+  console.log("Server is running in 3000");
+});
