2282a743ac8a4e8358fd9b68dbf92cedfde1ae37
Dawit Mellese <dawitm777@gmail.com>
2024-04-11 22:52:23 +0300

    full functional sign up page

--- client/src/pages/Signup.jsx
+++ client/src/pages/Signup.jsx
@@ -1,12 +1,54 @@
 import { Link } from "react-router-dom";
-import { Button, Label, TextInput } from "flowbite-react";
+import { Alert, Button, Label, Spinner, TextInput } from "flowbite-react";
+import { useState } from "react";
+import {useNavigate} from "react-router-dom"
 
 const Signup = () => {
+  const [formData, setFormData] = useState({});
+  const [errorMessage, setErrorMessage] = useState(null);
+  const [loading, setLoading] = useState(false);
+  const navigate = useNavigate()
+
+  const handleChange = (e) => {
+    setFormData({ ...formData, [e.target.id]: e.target.value.trim() });
+    console.log(formData);
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    if (!formData.username || !formData.email || !formData.password) {
+      return setErrorMessage("All fields are required.");
+    }
+    try {
+      setLoading(true);
+      setErrorMessage(null);
+      const res = await fetch("/api/auth/signup", {
+        method: "Post",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify(formData),
+      });
+      const data = await res.json();
+      console.log(data);
+      if (data.success === false) {
+        return setErrorMessage(data.message);
+      }
+
+      if(res.ok){
+        navigate('/sign-in')
+      }
+
+      setLoading(false);
+    } catch (error) {
+      setErrorMessage(error.message);
+      setLoading(false);
+    }
+  };
+
   return (
-    <div className="min-h-screen mt-20">
+    <div className="min-h-screen mt-20 font-serif">
       <div className="flex p-3 max-w-3xl mx-auto flex-col md:flex-row">
         {/* left */}
-        <div className="flex-1 font-mono">
+        <div className="flex-1">
           <Link to={"/"} className="text-4xl font-bold dark:text-white">
             <span className="px-2 py-1 bg-gradient-to-r from-indigo-500 to-pink-500 rounded-lg text-white">
               Dev&apos;s
@@ -18,21 +60,51 @@ const Signup = () => {
 
         {/* right */}
         <div className="flex-1">
-          <form action="" className="flex flex-col gap-4">
+          <form
+            action=""
+            className="flex flex-col gap-4"
+            onSubmit={handleSubmit}
+          >
             <div className="">
               <Label value="Your username" />
-              <TextInput type="text" placeholder="Username" id="username" />
+              <TextInput
+                type="text"
+                placeholder="Username"
+                id="username"
+                onChange={handleChange}
+              />
             </div>
             <div className="">
               <Label value="Your email" />
-              <TextInput type="email" placeholder="Email" id="email" />
+              <TextInput
+                type="email"
+                placeholder="Email"
+                id="email"
+                onChange={handleChange}
+              />
             </div>
             <div className="">
               <Label value="Your password" />
-              <TextInput type="password" placeholder="Password" id="password" />
+              <TextInput
+                type="password"
+                placeholder="Password"
+                id="password"
+                onChange={handleChange}
+              />
             </div>
-            <Button gradientDuoTone={"purpleToPink"} type="submit">
-              Sign up
+            <Button
+              gradientDuoTone={"purpleToPink"}
+              type="submit"
+              disabled={loading}
+            >
+              {loading ? (
+                <>
+                  <Spinner size="sm" />
+                  <span className="pl-3">Loading...</span>
+                </>
+              ) : (
+                "Signup"
+              )}
             </Button>
           </form>
           <div className="flex gap-2 text-sm mt-5">
@@ -43,6 +115,11 @@ const Signup = () => {
               </Link>
             </span>
           </div>
+          {errorMessage && (
+            <Alert className="mt-5" color="failure">
+              {errorMessage}
+            </Alert>
+          )}
         </div>
       </div>
     </div>
